2e0f6a5a98ab4c1ad3948a6eeb0f3a63bc285521
RishavMishra <rishav.mishra@rmgx.in>
2025-05-18 16:46:27 +0530

    Initiate Spring Config Server

---
+++ MicroConfigServer/src/test/java/com/learn/config/server/MicroLearnConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroLearnConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
